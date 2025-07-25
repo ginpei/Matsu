4d69a741e8f69977844d92f05fe064b1f30c47e0
Ginpei <ginpei@ginpei.dev>
2025-07-25 00:12:08 -0700

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Matsu
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
