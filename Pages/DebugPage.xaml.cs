1ce815ca35fecddc360e6173a41efca1d73accab
Ginpei <ginpei@ginpei.dev>
2025-11-07 00:28:42 -0800

    light and dark modes

--- Pages/DebugPage.xaml.cs
+++ Pages/DebugPage.xaml.cs
@@ -14,6 +14,7 @@ using Windows.Foundation;
 using Windows.Foundation.Collections;
 using ManagedNativeWifi;
 using Matsu.Lib.WiFi;
+using System.Diagnostics;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -27,17 +28,59 @@ namespace Matsu.Pages
     {
         readonly WifiStore wifiStore;
 
+        public string GreetingMessage { get; set; } = "Hello, WinUI 3!";
+
         public DebugPage()
         {
             InitializeComponent();
 
             wifiStore = new WifiStore();
             WifiNetworksPanel.DataContext = wifiStore;
+
+            if (this.XamlRoot?.Content is FrameworkElement rootElement)
+            {
+                CurrentThemeText.Text = rootElement.RequestedTheme.ToString();
+            }
         }
 
         private void MyButton_Click(object sender, RoutedEventArgs e)
         {
             // TODO
+        }
+
+        private void SystemThemeButton_Click(object sender, RoutedEventArgs e)
+        {
+            Debug.WriteLine("System");
+            GreetingMessage = "System";
+            CurrentThemeText.Text = "Sys";
+            // Set theme on the root content instead of Application.Current
+            if (this.XamlRoot?.Content is FrameworkElement rootElement)
+            {
+                rootElement.RequestedTheme = ElementTheme.Default;
+                CurrentThemeText.Text = rootElement.RequestedTheme.ToString();
+            }
+        }
+
+        private void LightThemeButton_Click(object sender, RoutedEventArgs e)
+        {
+            Debug.WriteLine("Light");
+            // Set theme on the root content instead of Application.Current
+            if (this.XamlRoot?.Content is FrameworkElement rootElement)
+            {
+                rootElement.RequestedTheme = ElementTheme.Light;
+                CurrentThemeText.Text = rootElement.RequestedTheme.ToString();
+            }
+        }
+
+        private void DarkThemeButton_Click(object sender, RoutedEventArgs e)
+        {
+            Debug.WriteLine("Dark");
+            // Set theme on the root content instead of Application.Current
+            if (this.XamlRoot?.Content is FrameworkElement rootElement)
+            {
+                rootElement.RequestedTheme = ElementTheme.Dark;
+                CurrentThemeText.Text = rootElement.RequestedTheme.ToString();
+            }
         }
     }
 }
