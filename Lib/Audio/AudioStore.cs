0312223bfb37ef707146132ddf4caf72519d7244
Ginpei <ginpei@ginpei.dev>
2025-11-08 13:40:52 -0800

    follow device changes

--- Lib/Audio/AudioStore.cs
+++ Lib/Audio/AudioStore.cs
@@ -1,12 +1,14 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using AudioSwitcher.AudioApi.CoreAudio;
+using CommunityToolkit.Mvvm.ComponentModel;
 using Microsoft.UI.Dispatching;
-using NAudio.CoreAudioApi;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Reactive.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.Devices.Input;
 
 namespace Matsu.Lib.Audio
 {
@@ -19,8 +21,8 @@ namespace Matsu.Lib.Audio
             set => SetProperty(ref _name, value);
         }
 
-        private float _volume;
-        public float Volume
+        private double _volume;
+        public double Volume
         {
             get => _volume;
             set => SetProperty(ref _volume, value);
@@ -33,31 +35,56 @@ namespace Matsu.Lib.Audio
             set => SetProperty(ref _isMuted, value);
         }
 
-        private MMDevice? device = null;
+        private CoreAudioController controller = new CoreAudioController();
+        private CoreAudioDevice device;
+        private IDisposable? volumeSubscription;
+
         private readonly DispatcherQueue dispatcher = DispatcherQueue.GetForCurrentThread();
 
         public AudioStore() {
-            device = new MMDeviceEnumerator().GetDefaultAudioEndpoint(DataFlow.Render, Role.Multimedia);
+            device = controller.DefaultPlaybackDevice;
+            ResetDevice(device);
 
-            Name = device.DeviceFriendlyName;
-            Volume = device.AudioEndpointVolume.MasterVolumeLevelScalar;
-            IsMuted = device.AudioEndpointVolume.Mute;
-            Debug.WriteLine($"Name: {Name} Volume: {Volume * 100}%  Mute: {IsMuted}");
+            controller.AudioDeviceChanged.Subscribe((e) =>
+            {
+                var newDevice = controller.DefaultPlaybackDevice;
+                if (newDevice != device)
+                {
+                    ResetDevice(newDevice);
+                    device = newDevice;
+                }
+            });
+        }
 
-            device.AudioEndpointVolume.OnVolumeNotification += (data) =>
+        private void ResetDevice(CoreAudioDevice device)
+        {
+            Debug.WriteLine($"DefaultChanged: {device.FullName}");
+
+            volumeSubscription?.Dispose();
+            volumeSubscription = device.VolumeChanged.Subscribe((e) =>
             {
                 dispatcher.TryEnqueue(() =>
                 {
-                    Volume = data.MasterVolume;
-                    IsMuted = data.Muted;
+                    Name = device.FullName;
+                    Volume = device.Volume;
+                    IsMuted = device.IsMuted;
                 });
-            };
+            });
+
+            dispatcher.TryEnqueue(() =>
+            {
+                Debug.WriteLine($"audio store: {device.FullName}");
+                Name = device.FullName;
+                Volume = device.Volume;
+                IsMuted = device.IsMuted;
+            });
         }
 
         public void Dispose()
         {
-            device?.Dispose();
-            device = null;
+            volumeSubscription?.Dispose();
+            //device?.Dispose();
+            controller.Dispose();
         }
 
     }
